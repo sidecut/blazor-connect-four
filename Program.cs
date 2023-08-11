420166b3436377e812f9fd19163db8f1fcebac2c
James Raden <james.raden@gmail.com>
2023-08-10 22:38:28 -0400

    Rename everything to ConnectFour

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using blazor_connect_four;
+using ConnectFour;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
